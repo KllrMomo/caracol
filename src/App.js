6eedec76ff18c9cdbb44ccd2e033f48142027d4d
Mayrin <mayrin.peredia@cetys.edu.mx>
2025-06-09 11:51:32 -0700

    Creacion de navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <navBar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/articulos" element={<ArticulosSearch />} />
+        <Route path="/articulos/:id" element={<ArtOpen />} /> {/* ruta dinámica */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/autor" element={<MoreAutores />} />
+      </Routes>
+    </Router>
   );
 }
 
