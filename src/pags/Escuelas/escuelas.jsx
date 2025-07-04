94d538d61525873cec4a3be68221ae7c022ab559
Mayrin <mayrin.peredia@cetys.edu.mx>
2025-07-04 14:58:12 -0700

    cambios segun retroalimentación

--- src/pags/Escuelas/escuelas.jsx
+++ src/pags/Escuelas/escuelas.jsx
@@ -2,6 +2,8 @@ import React, { useRef, useState } from "react";
 import {Container} from "@mui/material";
 import Header from "../../components/header.jsx";
 import Footer from "../../components/footer.jsx";
+import Filtro from "../../components/filtro.jsx";
+import Catalogoparaescuelas2025 from "../../docs/Catalogoparaescuelas2025.pdf";
 import './escuela.css';
 
 function Escuelas () {
@@ -39,20 +41,25 @@ function Escuelas () {
         <section className="portada">
           <img src="/images/Escuela.png" alt="Imagen Escuelas" className="imagen-portada"/>
           <div className="banner">
-            <p className="banner-texto1">¡Trae a tus estudiantes a vivir la experiencia Caracol!</p>
-            <p className="banner-texto2">Martes a Domingo <br /> 10:00 am a 5:00 pm</p>
+            <div className="banner-texto">
+              <h1 className="banner-texto1">¡Trae a tus estudiantes a <br /> vivir la experiencia <br /> Caracol!</h1>
+              <p className="banner-texto2">Martes a Domingo <br /> 9:00 am a 4:00 pm</p>
+            </div>
           </div>
         </section>
-        
+
+        <Filtro className="filtro" />
+
         <section className="visita-caracol">
           <img src="/images/VISITA-CARACOL.png" alt="Visita Caracol" className="visita"/>
-          <p className="texto-visita">Descubre todas nuestras salas de exhibicion con un recorrido completo del mundo</p>
+          <p className="texto-visita">Descubre las maravillas de la península de Baja California a través de nuestras salas de exhibición. </p>
           
           <div className="salas-container">
             <img src="/images/Sala-Tierra.png" alt="Sala de la Tierra" className="sala-tierra"/>
             <img src="/images/Sala-Cielo.png" alt="Sala del Cielo" className="sala-cielo"/>
             <img src="/images/Sala-Mar.png" alt="Sala del Mar" className="sala-mar"/>
             <img src="/images/Planetario.png" alt="Planetario" className="planetario"/>
+            <img src="/images/Dinosaurios.png" alt="Dinosaurios" className="dinosaurios"/>
           </div>
           <p className="texto-visita2">* Nuestro recorrido cuenta con una duración aproximada de 3 horas</p>
           <p className="texto-visita3">Y exploren con sensorama</p>
@@ -64,19 +71,9 @@ function Escuelas () {
 
               <p className="texto-sensorama2">Incluido en tu visita para escuelas con más de 180 alumnos</p>
             </div>
-
-            
           </div>
-          <img src="/images/PROGRAMA-RECORIDO.png" alt="Programa tu Recorrido" className="recorrido"/>
-          
-          <p className="texto-recorrido">
-            En horario de: <br />
-            Martes a Domingo <br />
-            9:00 am a 4:00 pm
-          </p>
 
-          <img src="/images/DISPONIBILIDAD-visita.png" alt="Disponibilidad" className="disponibilidad-visita"/>
-          <img src="/images/Disponibilidad-movil.png" alt="Calendario" className="disponibilidad-visita-movil"/>
+          <p className="recorrido">¡PROGRAMA TU RECORRIDO!</p>
         </section>
 
         <section className="ciencia-aire-libre">
@@ -86,38 +83,55 @@ function Escuelas () {
           <p className="texto-ciencia2">*Mínimo 30 alumnos</p>
 
           <div className="paseo-bahia-container">
-            <img src="/images/paseo-bahia.png" alt="Paseo por la Bahía" className="paseo-bahia"/>
-            <p className="titulo-paseo">PASEO EN LA BAHÍA</p>
-            <p className="texto-paseo1">Embárcate en una emocionante <br />experiencia en barco mientras <br /> observas y conoces las <br />maravillas del mar.</p>
-            <p className="texto-paseo2">Duración: <br /> 1.5 horas aprox.</p>
+            <img src="/images/Bahia.jpg" alt="Paseo por la Bahía" className="paseo-bahia" />
+            
+            <div className="paseo-texto">
+              <p className="titulo-paseo">PASEO EN LA BAHÍA</p>
+              <p className="texto-paseo1">
+                Embárcate en una emocionante experiencia en barco <br /> mientras
+                observas y conoces las maravillas del mar.
+              </p>
+              <p className="texto-paseo2">
+                <span className="duracion-label">DURACIÓN</span> 
+                <i className="ri-time-line"></i> 1.5 horas aprox.
+              </p>
+            </div>
           </div>
 
+
           <div  className="bufadora-container">
-            <img src="/images/Bufadora.png" alt="Bufadora" className="bufadora"/>
-            <p className="titulo-bufadora">LA BUFADORA DESDE EL MAR</p>
-            <p className="texto-bufadora1">Disfruta la mejor vista de la Bufadora mientras navegas y aprendes sobre las criaturas que habitan nuestras costas.</p>
-            <p className="texto-bufadora2">Duración: <br /> 4 horas aprox.</p>
+            <img src="/images/Bufadora.jpg" alt="Bufadora" className="bufadora"/>
+            
+            <div className="bufadora-texto">
+              <p className="titulo-bufadora">LA BUFADORA DESDE <br /> EL MAR</p>
+              <p className="texto-bufadora1">Disfruta la mejor vista de la Bufadora mientras navegas y <br />aprendes  sobre las criaturas que habitan nuestras costas.</p>
+              <p className="texto-bufadora2">
+                <span className="duracion-label">DURACIÓN</span> 
+                <i className="ri-time-line"></i> 4 horas aprox.
+              </p>
+            </div>
           </div>
 
           <div className="avistamiento-de-ballenas">
-            <img src="/images/Ballenas.png" alt="Avistamiento de Ballenas" className="ballenas"/>
-            <div className="texto-ballenas-container"> 
-              <p className="titulo-ballenas">AVISTAMIENTO DE BALLENAS</p>
-              <p className="texto-ballenas1">Conoce la flora y fauna de la región, mientras aprendes sobre los ecosistemas que nos rodean.</p>
-              <p className="texto-ballenas2">Duración: <br /> 1.5 horas aprox.</p>
+            <img src="/images/Ballenas.jpg" alt="Avistamiento de Ballenas" className="ballenas"/>
+            
+            <div className="ballenas-texto"> 
+              <p className="titulo-ballenas">AVISTAMIENTO DE <br />BALLENAS</p>
+              <p className="texto-ballenas1">Conoce la flora y fauna de la región, mientras aprendes sobre <br /> los ecosistemas que nos rodean.</p>
+              <p className="texto-ballenas2">
+                <span className="duracion-label">DURACIÓN</span> 
+                <i className="ri-time-line"></i> 1.5 horas aprox.
+              </p>
+              <p className="texto-ballenas2">
+                <i class="ri-search-line"></i> Únicamente en temporada de migración
+              </p>
             </div>
           </div>
-          
-          <img src="/images/SUMERGETE.png" alt="SUMERGETE" className="sumergete"/>
 
-          <img src="/images/SUMERGETE-movil.png" alt="SUMERGETE" className="sumergete-movil"/>
-
-          <img src="/images/DISPONIBILIDAD-ciencia.png" alt="Programa Ciencia al Aire Libre" className="disponibilidad-ciencia"/>
-
-          <img src="/images/Disponibilidad-ciencia-movil.png" alt="Programa Ciencia al Aire Libre" className="disponibilidad-ciencia-movil"/>
+          <p className="sumergete">Sumérgete en esta experiencia única y conecta con la naturaleza</p>
         </section>
 
-        <section className="final">
+        {/*<section className="final">
           <p className="texto-final">Todas nuestras exhibiciones/actividades están disponibles para los niveles de preescolar hasta universidad.</p>
 
           <img src="images/perder.png" alt="Nadie se tiene que perder de la diversión"  className="perder"/>
@@ -126,24 +140,37 @@ function Escuelas () {
 
           <p className="texto-final2">(restricciones aplican)</p>
 
-        </section>
+        </section>*/}
 
         <section className="mas-informacion">
           <p className="info1">Para más información no dudes en contactarnos</p>
           <div className="mas-informacion-container">
             <div className="telefono-text">
               <p className="tel">Teléfono</p>
-              <p className="tel1"><a  href="tel:6461770897">(646) 177-0897</a></p>
-              <p className="tel2"><a href="tel:6461521993">(646) 152-1993</a></p>
+              <p><a  href="tel:6461770897" className="tel1">(646) 177-0897</a></p>
+              <p><a href="tel:6461521993" className="tel2">(646) 152-1993</a></p>
             </div>
             <div className="correo-text">
               <p className="correo">Correo Electrónico</p>
-              <p className="correo1"><a href="mailto:paola@caracolbc.org">paola@caracolbc.org</a></p>
+              <p><a href="mailto:paola@caracolbc.org" className="correo1">paola@caracolbc.org</a></p>
             </div>
             <div className="whatsapp-text">
               <p className="whatsapp">WhatsApp</p>
               <p><a className="whattel" href="https://wa.me/526464130544"> (646) 413-0544</a></p>
             </div>
+          </div>
+        </section>
+
+        <section className="documentos">
+            <h3 className="doc-title">Documentos</h3>
+          <div className="documentos-container">
+              <div className="documento-item">
+                <p className="documento">Catalogo Oficial 2025</p>
+                <a href={Catalogoparaescuelas2025} target="_blank" className="download-link" rel="noopener noreferrer">
+                    <i className="ri-download-line"></i>
+                    Descargar
+                  </a>
+              </div>
           </div>
         </section>
 
