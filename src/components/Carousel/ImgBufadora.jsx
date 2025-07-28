dd812769e2e7696449e1db62fbe17f69f2e4d53d
Mayrin <mayrin.peredia@cetys.edu.mx>
2025-07-28 15:21:19 -0700

    Cambios de acuerdo a diseño

---
+++ src/components/Carousel/ImgBufadora.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import './Image.css';
+
+const images = [
+    { id: 1, url: "images/Bufadora 1.png"},
+    { id: 2, url: "/images/Bahia2.png"},
+]
+
+const ImgBufadora= () => {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const handlePreviousClick = () => {
+        setCurrentImageIndex(
+            currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1
+        );
+    };
+
+    const handleNextClick = () => {
+        setCurrentImageIndex((currentImageIndex + 1) % images.length);
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            handleNextClick();
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [currentImageIndex]);
+
+    return (
+        <section className= "carusel">
+            <div className="image-container">
+                <button className="nav-button left" onClick={handlePreviousClick}>
+                  <i className="ri-arrow-left-wide-line"></i>
+                </button>
+
+                {images.map((image, index) => (
+                    <img 
+                        src={image.url} 
+                        alt="images" 
+                        className={ currentImageIndex === index ? 'block' : 'hidden'}
+                        key={image.id} 
+                    />
+                ))}
+
+                <button className="nav-button right" onClick={handleNextClick}>
+                  <i className="ri-arrow-right-wide-line"></i>
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default ImgBufadora
